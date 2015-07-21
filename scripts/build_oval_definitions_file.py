5d7b1d2ddaf7e99636687a120e548255ce343daf
David Ries <ries@farnamhallventures.com>
2015-07-21 12:39:43 -0500

    adding script for web search

--- scripts/build_oval_definitions_file.py
+++ scripts/build_oval_definitions_file.py
@@ -42,7 +42,7 @@ def main():
     source_options.add_argument('--contributor', nargs='*', dest='contributors', metavar='NAME', help='filter by contributor(s)')
     source_options.add_argument('--organization', nargs='*', dest='organizations', metavar='NAME', help='filter by organization(s)')
     source_options.add_argument('--reference_id', nargs='*', dest='reference_ids', metavar='REFERENCE_ID', help='filter by reference ids, e.g. CVE-2015-3306')
-    output_options.add_argument('--all_definitions', default=False, action="store_true", help='include all definitions in the repository (do not specify any other filters)')
+    source_options.add_argument('--all_definitions', default=False, action="store_true", help='include all definitions in the repository (do not specify any other filters)')
     args = vars(parser.parse_args())
 
     # get definitions index
