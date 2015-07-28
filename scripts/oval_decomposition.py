2dd5539a0c3f634bc47dcf6f133c16d8af47adfc
GunnarEngelbach <Gunnar.Engelbach@ThreatGuard.com>
2015-07-28 10:57:29 -0400

    Revised test submissions by using actual submissions

--- scripts/oval_decomposition.py
+++ scripts/oval_decomposition.py
@@ -53,12 +53,12 @@ def main():
         
     
     if not oval.parseFromFile(filename):
-        print("Unable to parse source file '", filename, "':  no actions taken")
+        print("\n >> Unable to parse source file '{0}':  no actions taken".format(filename))
         return
 
     deflist = oval.getDefinitions()
     if not deflist or deflist is None or len(deflist) < 1:
-        print("Error:  this document does not contain any OVAL definitions.  No further action will be taken")
+        print("\n ## Error:  this document does not contain any OVAL definitions.  No further action will be taken")
         return
         
     if verbose:
@@ -113,6 +113,7 @@ def writeFile(path, element, verbose=False):
     
     if verbose:
         if os.path.exists(path):
+            # TODO  Determine if the element has not changed
             print("## Overwrite existing file: ", path)
         else:
             print("@@ Creating new file: ", path)
